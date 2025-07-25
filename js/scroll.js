6baaa6d04cb905c266e223471a88705eb116c575
dearsecret <dearsecret1217@gmail.com>
2025-07-25 17:02:41 +0900

    bottom fab

--- js/scroll.js
+++ js/scroll.js
@@ -7,15 +7,17 @@ window.addEventListener("scroll", () => {
   const currentScroll = window.scrollY;
   const hide = currentScroll > lastScroll + 5;
   const show = currentScroll < lastScroll - 5;
+  const footerHeight = footer.offsetHeight || 0;
+
 
   if (hide) {
     footer.classList.add("hide");
     tab.classList.add("hide");
-    fab.style.top = "10px"; 
+    fab.style.bottom = "10px"; 
   } else if (show) {
     footer.classList.remove("hide");
     tab.classList.remove("hide");
-    fab.style.top = "60px"; 
+    fab.style.bottom = `${footerHeight + 20}px`;
   }
 
   lastScroll = currentScroll;
